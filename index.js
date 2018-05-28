69e115d5a5871955393d519ec150874d1ebb55e1
e-barr <ebarrero04@gmail.com>
2018-05-28 00:31:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function theBeatlesPlay(arrayOfMusicians, arrayOfInstruments) {
   var emptyArray = []
 
   for (var i = 0; i < arrayOfMusicians.length; i ++) {
-    emptyArray.push(`"${arrayOfMusicians[i]} plays ${arrayOfInstruments[i]}"`)
+    emptyArray.push(`${arrayOfMusicians[i]} plays ${arrayOfInstruments[i]}`)
   }
 
   return emptyArray
